69b58b66a2d9d218a462686656890b726d9cfb24
otarachi <mr.dungnq@yahoo.com.vn>
2016-11-05 16:12:26 +0700

    Change assembly info

--- Vocabulearning/Properties/AssemblyInfo.cs
+++ Vocabulearning/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DemoApp")]
+[assembly: AssemblyTitle("Vocabulearning")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DemoApp")]
-[assembly: AssemblyCopyright("Copyright © 2011 by Simon B.")]
+[assembly: AssemblyProduct("Vocabulearning")]
+[assembly: AssemblyCopyright("Copyright © 2016 by Nguyễn Quang Dũng")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
